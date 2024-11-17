c8b7ff5ce54425220def48a104cc5fe6c400187e
yana-ilukhina <ilukhina.yana@gmail.com>
2024-11-17 18:11:15 +0300

    h7n1 remove duplicates in array

---
+++ src/main/java/ru/ilukhinayana/hw7/_Main/Main.java
@@ -0,0 +1,31 @@
+package ru.ilukhinayana.hw7._Main;
+
+import java.util.Arrays;
+/*1. Задачка на массивы: Дан массив целых чисел nums. Вам необходимо реализовать метод removeDuplicates(int[] nums).
+Он должен удалять дублирующие значения в массиве. Числа в исходном массиве находятся в порядке НЕУБЫВАНИЯ. Например,
+из массива [0,0,1,1,1,2,2,3,3,4] должен получится массив [0,1,2,3,4,0,0,0,0,0] При запуске этого класса, у вас
+должно вывестись true в консоль.*/
+
+class Solution {
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int[] expectedNums = {0, 1, 2, 3, 4, 0, 0, 0, 0, 0};
+        int[] result = removeDuplicates(nums);
+        System.out.print(Arrays.equals(result, expectedNums));
+    }
+
+    public static int[] removeDuplicates(int[] nums) { // вариант решения через новый массив
+        int[] result = new int[nums.length]; // по умолчанию в массиве 0 везде
+        result[0] = nums[0];// вдруг первое число не 0
+        int d = 1;
+        int ln = nums.length;
+        for (int i = 1; i < ln; i++) {
+            if (result[d - 1] != nums[i]) {
+                result[d] = nums[i];
+                d++;
+            }
+        }
+        for (int j : result) System.out.print(j + " ");
+        return result;
+    }
+}
